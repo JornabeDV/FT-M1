27e4fdd373108d522b04e1ae346d53d743deb026
JornabeDV <103864663+JornabeDV@users.noreply.github.com>
2023-07-06 08:46:51 -0300

    Mas Resoluciones HW3

--- 03-JavaScriptAvanzado-II/homework/homework.js
+++ 03-JavaScriptAvanzado-II/homework/homework.js
@@ -13,6 +13,8 @@ nuevoContador()     // 2
 const otroContador = counter()
 otroContador()      // 1
 otroContador()      // 2 */
+
+//EJERCICIO 1:
 function counter() {
 
   let counterCreado = 0;
@@ -25,46 +27,74 @@ function counter() {
 let counterFunction = counter();
 console.log(counterFunction());
 
-/* Ejercicio 2
-Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback 
-que recibe por parámetro (cb); es decir, que "recuerde" el resultado de cada operación que hace, de manera que, 
-al realizar una operación por segunda vez, se pueda obtener el resultado de esa "memoria" sin tener que efectuar 
-otra vez cálculos que ya se hicieron anteriormente.
+// /* Ejercicio 2
+// Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback 
+// que recibe por parámetro (cb); es decir, que "recuerde" el resultado de cada operación que hace, de manera que, 
+// al realizar una operación por segunda vez, se pueda obtener el resultado de esa "memoria" sin tener que efectuar 
+// otra vez cálculos que ya se hicieron anteriormente.
 
-- cacheFunction debe retornar una función. Esta función debe aceptar un argumento (arg) e invocar a cb con ese argumento; hecho eso, debe guardar el argumento junto con el resultado de la invocación (tip: usá un objeto donde cada propiedad sea el argumento, y su valor el resultado de la correspondiente invocación a cb) de manera que, la próxima vez que reciba el mismo argumento, no sea necesario volver a invocar a cb, porque el resultado estará guardado en la "memoria caché".
+// - cacheFunction debe retornar una función. Esta función debe aceptar un argumento (arg) e invocar a cb con ese argumento; hecho eso, debe guardar el argumento junto con el resultado de la invocación (tip: usá un objeto donde cada propiedad sea el argumento, y su valor el resultado de la correspondiente invocación a cb) de manera que, la próxima vez que reciba el mismo argumento, no sea necesario volver a invocar a cb, porque el resultado estará guardado en la "memoria caché".
 
-  Ejemplo:
-  function square(n){
-    return n * n
-  }
+//   Ejemplo:
+//   function square(n){
+//     return n * n
+//   }
 
-  const squareCache = cacheFunction(square)
+//   const squareCache = cacheFunction(square)
 
-  squareCache(5)    // invocará a square(5), almacenará el resultado y lo retornará
-  squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) */
+//   squareCache(5)    // invocará a square(5), almacenará el resultado y lo retornará
+//   squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) */
 
+//   function cacheFunction(cb) {
+//     const cache = {};
+  
+//     return function (arg) {
+//       if (cache.hasOwnProperty(arg)) {
+//         return cache[arg];
+//       } else {
+//         const result = cb(arg);
+//         cache[arg] = result;
+//         return result;
+//       }
+//     };
+//   }
+  
+//   function square(n) {
+//     return n * n;
+//   }
+//   const squareCache = cacheFunction(square);
+
+  
+//   console.log(squareCache(5));
+//   console.log(squareCache(5));
+
+
+  // RESOLUCION 2:
   function cacheFunction(cb) {
-    const cache = {};
-  
-    return function (arg) {
-      if (cache.hasOwnProperty(arg)) {
-        return cache[arg];
-      } else {
-        const result = cb(arg);
-        cache[arg] = result;
-        return result;
+    let objC = {}
+    return function(arg) {
+      if (objC.hasOwnProperty(arg)){
+        return objC[arg]
       }
-    };
+      objC[arg] = cb(arg)
+      return objC[arg]
+    }
   }
-  
-  function square(n) {
-    return n * n;
-  }
-  const squareCache = cacheFunction(square);
 
-  
-  console.log(squareCache(5));
-  console.log(squareCache(5));
+
+  // RESOLUCION 3:
+
+  // function cacheFunction(cd) {
+  //   let objC = {};
+  //   return function(arg) {
+  //     if(objC.hasOwnproperty(arg)){
+  //       return obj[arg];
+  //     }
+  //   const result = cb(arg);
+  //     newobjeto [arg] = result;
+  //   }
+  // }
+
 
 //----------------------------------------
 
@@ -109,10 +139,6 @@ function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
 let textoAsteriscos = crearCadena.bind(" ", "*", "*");
 let textoGuiones = crearCadena.bind(" ", "-", "-");
 let textoUnderscore = crearCadena.bind(" ", "_" , "_");
-
-textoAsteriscos();
-textoGuiones();
-textoUnderscore();
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
